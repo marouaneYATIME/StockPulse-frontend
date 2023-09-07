27e9470e89e1a8ecd1a2bb9d72cc7acbce731631
marouaneYATIME <marwaneyatime14@gmail.com>
2023-09-07 03:45:07 +0200

    Total value store

--- src/components/product/productSummary/ProductSummary.js
+++ src/components/product/productSummary/ProductSummary.js
@@ -4,13 +4,76 @@
  * Folder: frontend
  * file: ProductSummary.js
 */
+import React, { useEffect } from "react";
+import "./ProductSummary.scss";
+import { AiFillDollarCircle } from "react-icons/ai";
+import { BsCart4, BsCartX } from "react-icons/bs";
+import { BiCategory } from "react-icons/bi";
+import InfoBox from "../../infoBox/InfoBox";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  CALC_CATEGORY,
+  CALC_OUTOFSTOCK,
+  CALC_STORE_VALUE,
+  selectCategory,
+  selectOutOfStock,
+  selectTotalStoreValue,
+} from "../../../redux/features/product/productSlice";
 
-import React from 'react'
+// Icons
+const earningIcon = <AiFillDollarCircle size={40} color="#fff" />;
+const productIcon = <BsCart4 size={40} color="#fff" />;
+const categoryIcon = <BiCategory size={40} color="#fff" />;
+const outOfStockIcon = <BsCartX size={40} color="#fff" />;
 
-const ProductSummary = () => {
+// Format Amount
+export const formatNumbers = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+const ProductSummary = ({ products }) => {
+  const dispatch = useDispatch();
+  const totalStoreValue = useSelector(selectTotalStoreValue);
+//   const outOfStock = useSelector(selectOutOfStock);
+//   const category = useSelector(selectCategory);
+
+  useEffect(() => {
+    dispatch(CALC_STORE_VALUE(products));
+    // dispatch(CALC_OUTOFSTOCK(products));
+    // dispatch(CALC_CATEGORY(products));
+  }, [dispatch, products]);
+
   return (
-    <div>ProductSummary</div>
-  )
-}
+    <div className="product-summary">
+      <h3 className="--mt">Inventory Stats</h3>
+      <div className="info-summary">
+        <InfoBox
+          icon={productIcon}
+          title={"Produit Total"}
+          count={products.length}
+          bgColor="card1"
+        />
+        <InfoBox
+          icon={earningIcon}
+          title={"Valeur totale"}
+          count={`$${formatNumbers(totalStoreValue.toFixed(2))}  `}
+          bgColor="card2"
+        />
+        <InfoBox
+          icon={outOfStockIcon}
+          title={"En rupture"}
+          count={"0"}
+          bgColor="card3"
+        />
+        <InfoBox
+          icon={categoryIcon}
+          title={"Toutes "}
+          count={"0"}
+          bgColor="card4"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default ProductSummary
+export default ProductSummary;
--- src/redux/features/product/productSlice.js
+++ src/redux/features/product/productSlice.js
@@ -70,52 +70,62 @@ const productSlice = createSlice({
   initialState,
   reducers: {
     CALC_STORE_VALUE(state, action) {
-        console.log("store product")
-    }
+      const products = action.payload;
+      const array = [];
+      products.map((item) => {
+        const { price, quantity } = item;
+        const productValue = price * quantity;
+        return array.push(productValue);
+      });
+      const totalValue = array.reduce((a, b) => {
+        return a + b;
+      }, 0);
+      state.totalStoreValue = totalValue;
+    },
   },
   extraReducers: (builder) => {
     builder
-        // Cases for Create a product
-        .addCase(createProduct.pending, (state) => {
-            state.isLoading = true;
-        })
-        .addCase(createProduct.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.isSuccess = true;
-            state.isError = false;
-            console.log(action.payload);
-            state.products.push(action.payload);
-            toast.success("Produit ajouté avec succès !");
-        })
-        .addCase(createProduct.rejected, (state, action) => {
-            state.isLoading = false;
-            state.isError = true;
-            state.message = action.payload;
-            toast.error(action.payload);
-        })
+      // Cases for Create a product
+      .addCase(createProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createProduct.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.isError = false;
+        console.log(action.payload);
+        state.products.push(action.payload);
+        toast.success("Produit ajouté avec succès !");
+      })
+      .addCase(createProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        toast.error(action.payload);
+      })
         
-        //get product cases
-        .addCase(getProducts.pending, (state) => {
-            state.isLoading = true;
-        })
-        .addCase(getProducts.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.isSuccess = true;
-            state.isError = false;
-            console.log(action.payload);
-            state.products = action.payload;
-        })
-        .addCase(getProducts.rejected, (state, action) => {
-            state.isLoading = false;
-            state.isError = true;
-            state.message = action.payload;
-            toast.error(action.payload);
-        })
+      //get product cases
+      .addCase(getProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.isError = false;
+        console.log(action.payload);
+        state.products = action.payload;
+      })
+      .addCase(getProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        toast.error(action.payload);
+      })
   }
 });
 
 export const {CALC_STORE_VALUE} = productSlice.actions
 
 export const selectIsLoading = (state) => state.product.isLoading;
-
+export const selectTotalStoreValue = (state) => state.product.totalStoreValue;
 export default productSlice.reducer
