1ccb1c00cd61c323c94cea76847c8f1d463d45b0
marouaneYATIME <marwaneyatime14@gmail.com>
2023-09-07 12:13:15 +0200

    update product

--- src/pages/editProduct/EditProduct.js
+++ src/pages/editProduct/EditProduct.js
@@ -4,10 +4,10 @@
  * Folder: frontend
  * file: EditProduct.js
 */
-
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
+import Loader from "../../components/loader/Loader";
 import {
   getProduct,
   getProducts,
@@ -15,6 +15,7 @@ import {
   selectProduct,
   updateProduct,
 } from "../../redux/features/product/productSlice";
+import ProductForm from "../../components/product/productForm/ProductForm";
 
 
 function EditProduct() {
@@ -25,9 +26,76 @@ function EditProduct() {
 
   const productEdit = useSelector(selectProduct);
 
+
+  const [product, setProduct] = useState(productEdit);
+  const [productImage, setProductImage] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    dispatch(getProduct(id));
+  }, [dispatch, id]);
+
+  useEffect(() => {
+    setProduct(productEdit);
+
+    setImagePreview(
+      productEdit && productEdit.image ? `${productEdit.image.filePath}` : null
+    );
+
+    setDescription(
+      productEdit && productEdit.description ? productEdit.description : ""
+    );
+  }, [productEdit]);
+
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProduct({ ...product, [name]: value });
+  };
+
+  const handleImageChange = (e) => {
+    setProductImage(e.target.files[0]);
+    setImagePreview(URL.createObjectURL(e.target.files[0]));
+  };
+
+
+  const saveProduct = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", product?.name);
+
+    formData.append("category", product?.category);
+    formData.append("quantity", product?.quantity);
+    formData.append("price", product?.price);
+    formData.append("description", description);
+    if (productImage) {
+      formData.append("image", productImage);
+    }
+
+    console.log(...formData);
+
+    await dispatch(updateProduct({ id, formData }));
+    await dispatch(getProducts());
+    navigate("/dashboard");
+  };
+
   return (
-    <div>EditProduct</div>
-  )
-}
+    <div>
+      {isLoading && <Loader />}
+      <h3 className="--mt">Edit Product</h3>
+      <ProductForm
+        product={product}
+        productImage={productImage}
+        imagePreview={imagePreview}
+        description={description}
+        setDescription={setDescription}
+        handleInputChange={handleInputChange}
+        handleImageChange={handleImageChange}
+        saveProduct={saveProduct}
+      />
+    </div>
+  );
+};
 
 export default EditProduct
